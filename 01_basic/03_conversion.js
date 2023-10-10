2e9945defcee218e95c528d348273b50e9989e37
harun22407 <147406655+harun22407@users.noreply.github.com>
2023-10-10 12:57:44 +0000

    conversion of data type done.

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,104 @@
+// let score = "33abc";
+// console.log(typeof (score));
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+
+
+// let score2 = null;
+// console.log(typeof (score2));
+// let valueInNumber2 = Number(score2);
+
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2)
+
+
+
+
+
+
+// let score3 = undefined;
+// console.log(typeof (score3));
+// let valueInNumber3 = Number(score3);
+
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3)
+
+
+
+
+// let score4 = true;
+// console.log(typeof (score4));
+// let valueInNumber4 = Number(score4);
+
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4)
+
+
+
+// let score5 = false;
+// console.log(typeof (score5));
+// let valueInNumber5 = Number(score5);
+
+// console.log(typeof valueInNumber5);
+// console.log(valueInNumber5)
+
+
+
+
+
+
+
+
+
+
+
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+// let isLoggedIn2 = "";
+
+// let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(booleanIsLoggedIn2);
+
+
+
+// let isLoggedIn3 = "Harun";
+
+// let booleanIsLoggedIn3 = Boolean(isLoggedIn3)
+// console.log(booleanIsLoggedIn3);
+
+
+
+
+
+// let isLoggedIn4 = 5;
+
+// let booleanIsLoggedIn4 = Boolean(isLoggedIn4)
+// console.log(booleanIsLoggedIn4);
+
+
+
+
+
+
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+
+
+
